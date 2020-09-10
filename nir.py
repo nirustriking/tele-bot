b72253e79c3409e192dd412d54b25b09fe5627d3
nirustriking <70795625+nirustriking@users.noreply.github.com>
2020-09-10 13:01:32 +0530

    Update nir.py

--- nir.py
+++ nir.py
@@ -1,30 +1,44 @@
-x = "niruyg7" # ADAFRUIT_IO_USERNAME
-y = "aio_KhaX36hU7tSx28XnF2s2lbiJgSKP" # ADAFRUIT_IO_KEY 
-from Adafruit_IO import Client, Feed
-aio = Client(x,y)
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from Adafruit_IO import Client,Data,Feed
+import os
+def Turnoff(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned off")
+  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/7501809_preview.png')
+  send_value(0)
+def Turnon(update, context):
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Light turned on")
+  context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/507790_preview.png')
+  send_value(1)
+def send_value(value):
+  feed = aio.feeds('telebot')
+  aio.send_data(feed.key,value)
 
-feed=Feed(name='bot')
-result=aio.create_feed(feed)
-
-from Adafruit_IO import Data
-value=Data(value=1)
-value_send=aio.create_data('bot',value)
-
-
-from telegram.ext import Updater,CommandHandler
-import requests
-def grt_url():
-    contents = requests.get('https://random.dog/woof.json').json()
-    url = contents['url']
-    return url
-
-def dog(bot,update):
-    url=grt_url()
-    chat_id=update.message.chat_id
-    bot.send_photo(chat_id,photo=url)
-
-u = Updater('1305318094:AAFUjgQYIj4PFOlPjqNnWT_TZSUQ6R5BiP4')
-dp = u.dispatcher
-dp.add_handler(CommandHandler('dog',dog))
-u.start_polling()
-u.idle()
+def input_message(update, context):
+  text=update.message.text.upper()
+  text=update.message.text
+  if text == 'Turn on':
+    send_value(1)
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned on")
+    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/507790_preview.png')
+  elif text == 'Turn off':
+    send_value(0)
+    context.bot.send_message(chat_id=update.effective_chat.id,text="Light turned off")
+    context.bot.send_photo(chat_id=update.effective_chat.id,photo='https://i.dlpng.com/static/png/7501809_preview.png')
+def start(update,context):
+  start_message='''
+/Turnoff or 'Turn off':To turn off the light 
+/Turnon or 'Turn on'  :To turn on the light 
+'''
+  context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
+ADAFRUIT_IO_USERNAME = os.getenv('ADAFRUIT_IO_USERNAME')
+ADAFRUIT_IO_KEY = os.getenv('ADAFRUIT_IO_KEY')
+TOKEN = os.getenv('TOKEN')
+aio = Client('ADAFRUIT_IO_USERNAME','ADAFRUIT_IO_KEY')
+updater=Updater('TOKEN',use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('turnoff',turnoff))
+dispatcher.add_handler(CommandHandler('turnon',turnon))
+dispatcher.add_handler(CommandHandler('start',start))
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
+updater.start_polling()
+updater.idle()
